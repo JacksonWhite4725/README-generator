3946c249096eccf4d726171124a0e1776021464e
JacksonWhite4725 <jawh4725@colorado.edu>
2021-08-23 12:52:56 -0600

    Created question array, installed inquirer and fs

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+console.log('Hi! Welcome to the README generator!');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'appTitle',
+        message: 'What is this app called?\n',
+    }, {
+        type: 'input',
+        name: 'appPurpose',
+        message: 'What is this app used for?\n',
+    }, {
+        type: 'input',
+        name: 'appUsage',
+        message: 'How does someone use this app?\n',
+    }, {
+        type: 'input',
+        name: 'appInstall',
+        message: 'How is this app installed?\n',
+    }, {
+        type: 'input',
+        name: 'appIssues',
+        message: 'How does a user report issues?\n',
+    }, {
+        type: 'input',
+        name: 'appContributions',
+        message: 'How does a user make contributions to the app?\n',
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
